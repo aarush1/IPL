3996fea3f00abdb3c4a6f124487f1bebe3eedb72
aarush1 <aarushverma123@gmail.com>
2020-05-03 03:12:01 +0530

    2.0

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 const fs = require("fs");
 const csv = require("csvtojson");
 const matchesPlayedPerYear = require("./ipl/matchesPlayedPerYear");
+const matchesWon = require("./ipl/matchesWon");
+const mostWins = require("./ipl/mostWins");
+const bestEco = require("./ipl/bestEco");
+const extraRuns = require("./ipl/extraRuns");
+const tossEff = require("./ipl/tossEff");
+
+
 
 const MATCHES_FILE_PATH = "./csv_data/matches.csv";
+const DELIVERIES_FILE_PATH = "./csv_data/deliveries.csv";
 const JSON_OUTPUT_FILE_PATH = "./public/data.json";
 
 function main() {
   csv()
     .fromFile(MATCHES_FILE_PATH)
     .then(matches => {
-      let result = matchesPlayedPerYear(matches);
-      saveMatchesPlayedPerYear(result);
+      csv()
+        .fromFile(DELIVERIES_FILE_PATH)
+        .then(deliveries => {
+          let result = matchesPlayedPerYear(matches);
+          let result1 = matchesWon(matches);
+          let result2 = mostWins(matches);
+          let result3 = bestEco(matches,deliveries,"2015");
+          let result4 = extraRuns(matches,deliveries,"2016");
+          let result5 = tossEff(matches);
+
+          saveMatchesPlayedPerYear(result, result1, result2,result3,result4,result5);
+        });
     });
 }
 
-function saveMatchesPlayedPerYear(result) {
+
+
+function saveMatchesPlayedPerYear(result, result1, result2,result3,result4,result5) {
   const jsonData = {
-    matchesPlayedPerYear: result
+    matchesPlayedPerYear: result,
+    matchesWon: result1,
+    mostWins: result2,
+    bestEco: result3,
+    extraRuns: result4,
+    tossEff: result5
+
   };
+
   const jsonString = JSON.stringify(jsonData);
   fs.writeFile(JSON_OUTPUT_FILE_PATH, jsonString, "utf8", err => {
     if (err) {
--- public/app.js
+++ public/app.js
@@ -8,6 +8,11 @@ fetchAndVisualizeData();
 
 function visualizeData(data) {
   visualizeMatchesPlayedPerYear(data.matchesPlayedPerYear);
+  visualizeMatchesWon(data.matchesWon,data.mostWins);
+  visualizebestEco(data.bestEco);
+  visualizeextraRuns(data.extraRuns);
+  visualizetossEff(data.tossEff);
+  
   return;
 }
 
@@ -22,7 +27,7 @@ function visualizeMatchesPlayedPerYear(matchesPlayedPerYear) {
       type: "column"
     },
     title: {
-      text: "Matches Played Per Year"
+      text: "1.Matches Played Per Year"
     },
     subtitle: {
       text:
@@ -45,3 +50,184 @@ function visualizeMatchesPlayedPerYear(matchesPlayedPerYear) {
     ]
   });
 }
+
+
+
+
+function   visualizeMatchesWon(matchesWon,mostWins){
+
+  teams=Object.keys(mostWins);
+  seasons=Object.keys(matchesWon);
+  seriesData=[];
+  seriesData = teams.map(team => ({
+    name:team,
+    data:seasons.map(season => matchesWon[season][team]|| 0)
+  }));
+  console.log(seriesData);
+
+
+
+
+
+Highcharts.chart('container2', {
+  chart: {
+      type: 'column'
+  },
+  title: {
+      text: '2.Number of matches won by each team over all the years of IPL'
+  },
+  subtitle: {
+      text: 'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+
+  },
+  xAxis: {
+      categories: teams,
+      crosshair: true
+  },
+  yAxis: {
+      min: 0,
+      title: {
+          text: 'no_of_matcheswon'
+      }
+  },
+  tooltip: {
+      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+          '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+      footerFormat: '</table>',
+      shared: true,
+      useHTML: true
+  },
+  plotOptions: {
+      column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+      }
+  },
+  series : seriesData
+    });
+}
+
+
+function visualizebestEco(bestEco) {
+  const seriesData = [];
+  for (let year in bestEco) {
+    seriesData.push([year, bestEco[year]]);
+  }
+
+  Highcharts.chart("container3", {
+    chart: {
+      type: "column"
+    },
+    title: {
+      text: "3.Top 10 economical bowlers in 2015"
+    },
+    subtitle: {
+      text:
+        'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+    },
+    xAxis: {
+      type: "category"
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: "Player"
+      }
+    },
+    series: [
+      {
+        name: "Economy",
+        data: seriesData
+      }
+    ]
+  });
+}
+
+  function visualizeextraRuns(extraRuns) {
+    const seriesData = [];
+    for (let year in extraRuns) {
+      seriesData.push([year, extraRuns[year]]);
+    }
+  
+    Highcharts.chart("container4", {
+      chart: {
+        type: "column"
+      },
+      title: {
+        text: "4.Extra runs conceded by each team in 2016"
+      },
+      subtitle: {
+        text:
+          'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+      },
+      xAxis: {
+        type: "category"
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: "Runs"
+        }
+      },
+      series: [
+        {
+          name: "Runs",
+          data: seriesData
+        }
+      ]
+    });
+  }
+
+  function visualizetossEff(tossEff)
+  {
+
+    teams=Object.keys(tossEff);
+    seasons=["field","bat"];
+    seriesData=[];
+    seriesData = seasons.map(season => ({
+      name:season,
+      data:teams.map(team => tossEff[team][season]|| 0)
+    }));
+    console.log(seriesData);
+
+    Highcharts.chart("container5", {
+      chart: {
+        type: "bar"
+      },
+      title: {
+        text: "5.Toss Decision VS Wins"
+      },
+      subtitle: {
+        text:
+          'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+      },
+      xAxis: {
+        categories: teams
+      },
+      yAxis: {
+        allowDecimals: false,
+        title: {
+          text: "Total Wins"
+        }
+      },
+      legend: {
+        reversed: true
+      },
+      plotOptions: {
+        series: {
+          stacking: "normal"
+        }
+      },
+      series: seriesData
+    });
+  
+  }
+
+
+
+
+
+
+
+
